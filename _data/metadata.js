e7b8fbc5ca9b241f9497a6c81b32c237651037ef
pomonella <39027165+pmnlla@users.noreply.github.com>
2025-10-27 15:27:03 -0400

    Add Tailwind CSS styles and configuration for development environment
    - Created a new Tailwind CSS file with base styles, utilities, and theme settings.
    - Defined custom properties for colors, fonts, and spacing.
    - Added responsive design utilities for margins, paddings, and flexbox.
    - Introduced a mise.toml configuration file for managing development tools and tasks.
    - Set up tasks for installing dependencies, building the site, and starting the development server.
    - Restyle templates, add Tailwind, start migrating off hardcoded css.

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "👋",
+	url: "https://lilaac.net/blog",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Because a jack of all trades is a master of none, but oftentimes better than a master of none.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Itai Shalev",
+		email: "itai@lilaac.net",
+		url: "https://lilaac.net/"
 	}
 }
